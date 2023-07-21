cb8c7d51b8420b580c845fb0e5391e0baec43709
Andrey Gladkiy <68944387+AndrewSmooth@users.noreply.github.com>
2023-07-21 14:00:33 +0500

    ...

--- sportshop/sportshop/settings.py
+++ sportshop/sportshop/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7&0k(@nxhy)29(jvk@k9-@3@-emtp*i1m9j$l+r@v9-+-li45j'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
